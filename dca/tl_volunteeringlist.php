188d4996a8b3a550e337c2307f32552954557f9f
Frank Hoppe <hoppe.berlin@googlemail.com>
2019-08-09 10:22:25 +0200

    String durch StringUtil ersetzt

--- dca/tl_volunteeringlist.php
+++ dca/tl_volunteeringlist.php
@@ -238,7 +238,7 @@ class tl_volunteeringlist extends Backend
 		if ($varValue == '')
 		{
 			$autoAlias = true;
-			$varValue = standardize(String::restoreBasicEntities($dc->activeRecord->title));
+			$varValue = standardize(\StringUtil::restoreBasicEntities($dc->activeRecord->title));
 		}
 
 		$objAlias = $this->Database->prepare("SELECT id FROM tl_volunteeringlist WHERE alias=?")
